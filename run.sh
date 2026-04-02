af21750347fbf4c9fb4d290a046f21fdb5437dc7
Bryan Anderson <shadow431@gmail.com>
2026-04-01 21:11:36 -0700

    run.sh updates

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-for menu in meals occasions desserts; do  docker run --rm --add-host=host.docker.internal:host-gateway --env-file .env --env-file .env-$menu menuparser:20260401; done
+for menu in meals occasions desserts; do  docker run --rm --add-host=host.docker.internal:host-gateway --env-file .env-menuparser --env-file .env-$menu menuparser:20260401; done
